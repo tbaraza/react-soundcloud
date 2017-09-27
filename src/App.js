6e482c31b27eda9a66481b73021b036be7481b13
Tonida Baraza <tonida.baraza@andela.com>
2017-09-27 18:46:44 +0300

    test use of ant design components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Button } from 'antd';
 import logo from './logo.svg';
+import 'antd/lib/button/style/css'; 
 import './App.css';
 
 class App extends Component {
@@ -13,6 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button>Hello</Button>
       </div>
     );
   }
